f27b4556acfe72ba2afaf070d45073260597e46b
hudayfa2 <hudayfaciftci@users.noreply.github.com>
2020-01-15 04:48:00 +0300

    BubbleSort.java update

--- DataStructuresJava/src/Sorts/BubbleSort.java
+++ DataStructuresJava/src/Sorts/BubbleSort.java
@@ -18,7 +18,7 @@ public class BubbleSort {
   
     }  
     public static void main(String[] args) {  
-                int arr[] ={3,60,35,2,45,320,5};  
+                int arr[] ={3,78,35,41,45,320,5};  
                  
                 System.out.println("Dizi Oncesi Bubble Sort");  
                 for(int i=0; i < arr.length; i++){  
